e590cda4c9cdf7cb6d2d7c69139f7617bde2e73b
Emily Kristin Garcia <emily.garcia@brw.local>
2026-05-12 11:49:09 -0300

    Adicionando a pasta Classes e composicao

---
+++ Classes e composicao/livro_2.py
@@ -0,0 +1,24 @@
+# 1) Crie uma classe chamada Livro com um construtor que aceita os parâmetros titulo, autor e ano_publicacao. Inicie um atributo chamado disponivel como True por padrão.
+class Livro:
+    def __init__(self, titulo, autor, ano_publicacao):
+        self.titulo = titulo
+        self.autor = autor
+        self.ano_publicacao = ano_publicacao
+        self.disponivel = True
+
+    def __str__(self):
+        return f"Livro: {self.titulo} | Autor: {self.autor} | Ano de Publicação: {self.ano_publicacao}"
+
+livro1 = Livro("Aprendendo Python", "John Doe", 2022)
+livro2 = Livro("Data Science Fundamentals", "Jane Smith", 2020)
+
+print(livro1)
+print(livro2)
+
+    def emprestar(self):
+        self.disponivel = False
+
+livro3 = Livro("Python Cookbook", "Samuel Developer", 2019)
+print(f"Antes de emprestar: Livro disponível? {livro3.disponivel}")
+livro3.emprestar()
+print(f"Depois de emprestar: Livro disponível? {livro3.disponivel}")
